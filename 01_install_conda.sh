f959a562d2630ea951ea96a383d4dff6cfc2e62e
Thach.Le <thachln@mks.com.vn>
2024-05-03 10:10:09 +0700

    Try to upgrade version of: - Torch 2.2.1 with Cuda 1.2.1 - torchvision==0.17.1+cu121

--- 01_install_conda.sh
+++ 01_install_conda.sh
@@ -8,8 +8,9 @@ conda activate openmmlab
 call .venv\Scripts\activate
 
 REM pip install -r requirements.txt
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121-
 pip install torchvision==0.17.1+cu121 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install torch==2.2.1+cu121 torchaudio==2.2.1+cu121 torch==2.2.1+cu121 torchvision==0.17.1+cu121 -f https://download.pytorch.org/whl/torch_stable.html
 
 pip install -U openmim
 mim install mmengine
